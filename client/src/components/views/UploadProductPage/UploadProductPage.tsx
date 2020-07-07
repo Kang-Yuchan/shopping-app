7fc353dcaadfbccc6908ea5aec564aacddc0e349
Yuchan <mikuplayer123@gmail.com>
2020-07-07 16:40:51 +0900

    select option

--- client/src/components/views/UploadProductPage/UploadProductPage.tsx
+++ client/src/components/views/UploadProductPage/UploadProductPage.tsx
@@ -1,6 +1,5 @@
 import * as React from "react";
 import { Typography, Button, Input } from "antd";
-import { FaCode } from "react-icons/fa";
 import { useSelector } from "react-redux";
 import styled from "styled-components";
 import { RootState } from "../../../_reducer";
@@ -16,14 +15,38 @@ const TitleContainer = styled.div`
   text-align: center;
   margin-bottom: 2rem;
 `;
+const { Title } = Typography;
+const { TextArea } = Input;
+
+const SelectOptions: Array<{ key: number; value: string }> = [
+  {
+    key: 1,
+    value: "Mouse"
+  },
+  {
+    key: 2,
+    value: "Keyboard"
+  },
+  {
+    key: 3,
+    value: "Mouse Pad"
+  },
+  {
+    key: 4,
+    value: "Monitor"
+  },
+  {
+    key: 5,
+    value: "Audio"
+  }
+];
 
 const UploadProductPage: React.FC<ComponentsProps> = props => {
   const [title, onChangeTitle] = useInput("");
   const [text, onChangeText] = useInput("");
   const [price, onChangePrice] = useInput(0);
+  const [option, onChangeOption] = useInput(0);
   const { me } = useSelector((state: RootState) => state.user);
-  const { Title } = Typography;
-  const { TextArea } = Input;
 
   React.useEffect(() => {
     if (me?.isAuth === false) {
@@ -52,7 +75,13 @@ const UploadProductPage: React.FC<ComponentsProps> = props => {
         <Input type='number' onChange={onChangePrice} value={price} />
         <br />
         <br />
-        <select></select>
+        <select onChange={onChangeOption} value={option}>
+          {SelectOptions.map(item => (
+            <option key={item.key} value={item.value}>
+              {item.value}
+            </option>
+          ))}
+        </select>
         <br />
         <br />
 
